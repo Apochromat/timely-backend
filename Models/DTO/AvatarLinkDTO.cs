4cc29a4c519e733ab6f5b2fa2c78541d95b75d6a
Stepan Ustimenko <50939552+Apochromat@users.noreply.github.com>
2023-03-08 18:25:38 +0700

    fix(avatar): исправил модель аватара

--- Models/DTO/AvatarLinkDTO.cs
+++ Models/DTO/AvatarLinkDTO.cs
@@ -4,5 +4,5 @@ using System.ComponentModel.DataAnnotations;
 namespace timely_backend.Models.DTO;
 
 public class AvatarLinkDTO {
-    [DisplayName("avatarLink")] [Required] [RegularExpression(@"\bhttps?:\/\/\S+\.(?:jpg|jpeg|gif|png|bmp)\b")] public String AvatarLink { get; set; }
+    [DisplayName("avatarLink")] [Required] [RegularExpression(@"\bhttps?:\/\/\S+\.(?:jpg|jpeg|png|bmp)\b", ErrorMessage = "Неправильный тип ссылки")] public String AvatarLink { get; set; }
 }
