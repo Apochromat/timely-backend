1015f7c6d80f8e23018d75ef503f55e4618215a8
Stepan Ustimenko <50939552+Apochromat@users.noreply.github.com>
2023-03-08 18:15:56 +0700

    hotfix(avatar)

--- Models/DTO/AvatarLinkDTO.cs
+++ Models/DTO/AvatarLinkDTO.cs
@@ -4,5 +4,5 @@ using System.ComponentModel.DataAnnotations;
 namespace timely_backend.Models.DTO;
 
 public class AvatarLinkDTO {
-    [DisplayName("avatarLink")] [Required] public String AvatarLink { get; set; }
+    [DisplayName("avatarLink")] [Required] [RegularExpression(@"\bhttps?:\/\/\S+\.(?:jpg|jpeg|gif|png|bmp)\b")] public String AvatarLink { get; set; }
 }
